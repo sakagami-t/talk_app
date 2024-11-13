4b1eacb02803f9994d245266f3e943f2b872d6b3
sakagami-t <sakagamit@nihonsoft.co.jp>
2024-11-13 22:17:14 +0900

    Add message fetching functionality to `MessageScreen`
    * Import `dart:convert` and `http` packages
    * Add `_fetchMessages` function to fetch data from the specified URL and parse the response
    * Populate `_messages` list with the fetched messages
    * Call `_fetchMessages` in `initState` to load messages when the screen is initialized

--- lib/page/message.dart
+++ lib/page/message.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 class MessageScreen extends StatefulWidget {
   const MessageScreen({super.key});
@@ -10,6 +12,30 @@ class MessageScreen extends StatefulWidget {
 class MessageScreenState extends State<MessageScreen> {
   final List<String> _messages = [];
   final TextEditingController _controller = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchMessages();
+  }
+
+  Future<void> _fetchMessages() async {
+    final response = await http.get(Uri.parse(
+        'https://q3a6g0zjk9.execute-api.ap-northeast-1.amazonaws.com/conversationHistory?user_id=a&conversation_id=bb141ab3-72bb-3369-ed52-aaebd12dbfbf'));
+
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      final List<dynamic> messages = data['messages'];
+      setState(() {
+        _messages.clear();
+        for (var message in messages) {
+          _messages.add(message['content']);
+        }
+      });
+    } else {
+      throw Exception('Failed to load messages');
+    }
+  }
 
   void _sendMessage() {
     if (_controller.text.isNotEmpty) {
